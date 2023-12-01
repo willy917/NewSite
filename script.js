2122890c4db59bf5cc3fb49543be932a49dd5516
willy917 <wilrobles917@hotmail.com>
2023-11-30 20:35:13 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//alert("Hola este es mi Javascript");
+let nombre="William"; //permite modificar la variable
+//nombre="Elia";
+var nombre1 = "Robles"; //permite modificar la variable
+//nombre1="Elia";
+const nombre2="Villamil"; //no se le puede asignar otro valor a una constante
+//nombre2="Elia";
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
+
+//SELECCIONAR ELEMENTOS
+let contenidoTitulo="Mi titulo 1";
+let titulo=document.querySelector(".logo .fuente-")
+titulo,innerhtml= contenidoTitulo;
+
+//CONDICIONALES
+
+if(nombre=="William"){
+    console.log(nombre="Elia");
+}
+else if(nombre=="Pedro"){
+    console.log("Daniela");
+}
---
+++ script1.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(){
+    let menu_responsive = document.querySelector(".checkbtn");
+    menu_responsive.onclick = function () {
+      navBar = document.querySelector(".navbar");
+      navBar.classList.toggle("active");
+    };});
